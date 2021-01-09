80809ba3bbb9d918be1c3a7c4bd339911bdc6515
Petr <musakius.work@gmail.com>
2021-01-09 11:41:07 +0300

    feat: add settings object for page

--- client/src/pages/signInUp/signInUp.tsx
+++ client/src/pages/signInUp/signInUp.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import {Link /* , Redirect */} from 'react-router-dom';
 
 import classes from './signInUp.module.scss';
 
@@ -6,10 +7,39 @@ type SignInUpProps = {
   type: string
 }
 
-const SignInUp: React.FC<SignInUpProps> = (type) => {
+const SignInUp: React.FC<SignInUpProps> = ({type}) => {
   useEffect(() => {
     console.log(type);
   }, [type]);
+
+  const settings = ((type) => {
+    switch (type) {
+      case 'login':
+        return {
+          head: 'Вход в Trello',
+          linkWord: 'Зарегистрировать аккаунт',
+          linkTo: 'register',
+          isUsername: false,
+          btn: {
+            name: 'Войти',
+            bg: 'green'
+          }
+        };
+      case 'register':
+        return {
+          head: 'Регистрация аккаунта',
+          linkWord: 'Уже есть аккаунт? Войти',
+          linkTo: 'login',
+          isUsername: true,
+          btn: {
+            name: 'Зарегистрироваться',
+            bg: 'blue'
+          }
+        };
+      default:
+        throw new Error('type of sign_IN_UP is not valid');
+    }
+  })(type);
 
   return (
     <>
@@ -22,6 +52,11 @@ const SignInUp: React.FC<SignInUpProps> = (type) => {
       </header>
       <main className={classes.main}>
         <section className={classes.container}>
+        <h1 className={classes.title}>{settings.head}</h1>
+          <hr className={classes.borderHR}/>
+          <Link className={classes.link} to={`/${settings.linkTo}`}>
+            {settings.linkWord}
+          </Link>
         </section>
       </main>
     </>
